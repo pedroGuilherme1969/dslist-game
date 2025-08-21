168084f3409ee30cd6223701833a861fb4609a59
Pedro <pg20041312@gmail.com>
2025-08-20 23:34:11 -0300

    projections, games by list

--- src/main/java/com/games/dslist/services/GameService.java
+++ src/main/java/com/games/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.games.dslist.services;
 import com.games.dslist.dto.GameDTO;
 import com.games.dslist.dto.GameMinDTO;
 import com.games.dslist.entities.Game;
+import com.games.dslist.projections.GameMinProjection;
 import com.games.dslist.repositories.GameRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
